12b24a44a06d699a8040303aebb8be3cb4eebd16
Bruno Nascimento <sunr00t@gmail.com>
2023-06-16 22:34:18 -0300

    update flask debug level

--- server.py
+++ server.py
@@ -21,12 +21,12 @@ def flask():
     s = json.dumps(content['skewness'])
     e = json.dumps(content['entropy'])
     predict = ia.user_entries(v, c, s, e)
-    print("HOST: ",request.remote_addr, "Predict: " + str(predict))
-    return json.dumps(predict, default=str)
+    print("HOST:",request.remote_addr, "| Predict: " + str(predict))
+    return str(predict)
 
 if __name__ == "__main__":
   app.run(debug=True, host='0.0.0.0', port=5000)
   
 logging.basicConfig()
-logging.getLogger('flask_cors').level = logging.DEBUG
+logging.getLogger('flask_cors').level = logging.INFO
   
